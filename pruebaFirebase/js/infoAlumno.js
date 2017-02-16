23e8ab737c1a614cd5ef0bb1384d7c12156e67e0
WorrenGilgamesh <victor.oz@live.com>
2017-02-16 12:10:38 -0600

    conexion bd forma alumno proceso

---
+++ pruebaFirebase/js/infoAlumno.js
@@ -0,0 +1,141 @@
+
+
+
+
+//LLenado de base de datos
+
+function llenaAlumno(nom,cU,tel,car,semestre,interes, interes2, interes3){
+
+ var res = false;
+ var nombre;
+ var claveU;
+ var telefono;
+ var carrera;
+ var semestreAprox;
+ var empresaInteres;
+ var empresaInteres1;
+ var empresaInteres2;
+
+  	// nombre alumno
+  	if(nom != null)
+  	{
+  		nombre = nom;
+  		res = true;
+  	}
+  	else
+  	{
+  		document.getElementById("txtAlumNom").innerHTML = "Nombre del alumno invalido";
+  	}
+  	//Clave unica
+  	if( cU != null)
+  	{
+  		claveU = cU;
+  		res = true == res;
+  	}
+  	else
+  	{
+  		document.getElementById("txtAlumCU").innerHTML = "Clave unica invalida";
+  	}
+  	//número de alumno
+  	if( tel != null)
+  	{
+  		telefono = tel;
+  		res = true == res;
+  	}
+  	else
+  	{
+  		document.getElementById("txtAlumTel").innerHTML = "Ingresa un número valido, solo números";
+  	}
+  	//carrera
+  	if( car != null)
+  	{
+  	carrera = car;
+  		res = true == res;
+  	}
+  	else
+  	{
+  		document.getElementById("txtAlumCarrera").innerHTML = "Ingresa una carrera valida";
+  	}
+  	//semestre aproximado
+  	if(semestre != null && semestre > 0)
+  	{
+  	semestreAprox =semestre;
+  		res = true == res;
+  	}
+  	else
+  	{
+  		document.getElementById("txtAlumSem").innerHTML = "Ingresa unsemestre aproximado mayor de 0";
+  	}
+    //Empresa interes
+    if(interes != null)
+    {
+    empresaInteres =interes;
+      res = true == res;
+    }
+    else
+    {
+      document.getElementById("txtAlumInteres1").innerHTML = "Ingresa tu empresa de interes";
+    }
+    if(interes != null)
+    {
+    empresaInteres2 =interes2;
+      res = true == res;
+    }
+    else
+    {
+      document.getElementById("txtAlumInteres2").innerHTML = "Ingresa tu empresa de interes";
+    }
+    if(interes != null)
+    {
+    empresaInteres3 =interes3;
+      res = true == res;
+    }
+    else
+    {
+      document.getElementById("txtAlumInteres3").innerHTML = "Ingresa tu empresa de interes";
+    }
+
+
+  	if(res)
+  	{
+  		var uid = firebase.auth().currentUser.uid;
+  		var email = firebase.auth().currentUser.email;
+  		const urlAlum = "/alumnos/" + uid + "/";
+  		const urlUsers = "/users/" + uid + "/";
+  		
+
+      firebase.auth().currentUser.updateProfile({
+        displayName: nombre
+      }).then(function() {
+		  // Update successful.
+      firebase.database().ref(urlEmpresa).set({
+        Nombre: nombre,
+        Email : email,
+        Contacto: claveU,
+        Telefono: telefono,
+        Carrera: carrera,
+        Semestre: semestre,
+        Empresa1: empresaInteres,
+        Empresa2: empresaInteres2,
+        Empresa3: empresaInteres3
+      }).then(function(){
+       firebase.database().ref(urlUsers).set({
+        Tipo : "Alumno",
+        Registro : "true",
+        Nombre : nombre
+      }).then(function(){
+        firebase.auth().currentUser.sendEmailVerification().then(function(){
+         const location = (window.location.href).replace("/mainAlumno.html","");
+         window.location.href = (location + "/verificacion.html");
+       })
+      })
+    })
+    }, function(error) {
+		  // An error happened.
+      console.log(error.message);
+    });
+
+    }
+
+  }
+  
--- pruebaFirebase/js/main.js
+++ pruebaFirebase/js/main.js
@@ -18,9 +18,9 @@ var config = {
   };
   */
 
-
-/*lOG OUT PROCESS*/
-btnLogOut.addEventListener("click", function(){
+  $(document).ready(function(){
+    /*lOG OUT PROCESS*/
+    btnLogOut.addEventListener("click", function(){
       firebase.auth().signOut().then(function() {
         console.log("Sign-out successful");
 
@@ -28,56 +28,81 @@ btnLogOut.addEventListener("click", function(){
         console.log("An error happened.");
         alert(error.message);
       });
-});
+    });
 
 
-function displayIdioma() {
-    var strIdioma = $("#selectAlumIdiomas :selected").text();
-    var btn = document.createTextNode(" de " + strIdioma);
-    var btn2 ="";
-    if (btn!=btn2) {
-    var btn2 = btn
-    
-    
-    var node = document.createElement("INPUT");               
-    var att = document.createAttribute("class");       
-    att.value = "w3-input";
-    node.setAttributeNode(att);
-    var att = document.createAttribute("type");       
-    att.value = "text";
-    node.setAttributeNode(att);
-    var att = document.createAttribute("id");       
-    att.value = "requiredNivel";
-    node.setAttributeNode(att);
-    var textnode = document.createTextNode(btn);        
-    node.appendChild(textnode);                             
-    document.getElementById("txtIdiomaNivel").appendChild(node);
+    function displayIdioma() {
+      var strIdioma = $("#selectAlumIdiomas :selected").text();
+      var btn = document.createTextNode(" de " + strIdioma);
+      var btn2 ="";
+      if (btn!=btn2) {
+        var btn2 = btn
 
-    var node = document.createElement("LABEL");               
-    var att = document.createAttribute("class");       
-    att.value = "w3-label w3-validate";
-    node.setAttributeNode(att);
-    var att = document.createAttribute("id");  
 
-    function randomString(length, chars) {
-    var result = '';
-    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-    return result;}
+        var node = document.createElement("INPUT");               
+        var att = document.createAttribute("class");       
+        att.value = "w3-input";
+        node.setAttributeNode(att);
+        var att = document.createAttribute("type");       
+        att.value = "text";
+        node.setAttributeNode(att);
+        var att = document.createAttribute("id");       
+        att.value = "requiredNivel";
+        node.setAttributeNode(att);
+        var textnode = document.createTextNode(btn);        
+        node.appendChild(textnode);                             
+        document.getElementById("txtIdiomaNivel").appendChild(node);
 
-    var id = randomString(3, '0123456789');
+        var node = document.createElement("LABEL");               
+        var att = document.createAttribute("class");       
+        att.value = "w3-label w3-validate";
+        node.setAttributeNode(att);
+        var att = document.createAttribute("id");  
 
-    att.value = "lbNivel" + id;
-    var nivel = "lbNivel" + id;
-    node.setAttributeNode(att);
-    var textnode = document.createTextNode(btn);
-    node.appendChild(textnode); 
-    document.getElementById("txtIdiomaNivel").appendChild(node);    
-    document.getElementById("txtIdiomaNivel").appendChild(btn);
-    document.getElementById(nivel).innerHTML = "Nivel";
-    document.getElementById("requiredNivel").required = true; 
+        function randomString(length, chars) {
+          var result = '';
+          for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+            return result;}
+
+          var id = randomString(3, '0123456789');
+
+          att.value = "lbNivel" + id;
+          var nivel = "lbNivel" + id;
+          node.setAttributeNode(att);
+          var textnode = document.createTextNode(btn);
+          node.appendChild(textnode); 
+          document.getElementById("txtIdiomaNivel").appendChild(node);    
+          document.getElementById("txtIdiomaNivel").appendChild(btn);
+          document.getElementById(nivel).innerHTML = "Nivel";
+          document.getElementById("requiredNivel").required = true; 
+        }
+      }
+      function displayMensaje() {
+        window.location.href = ("http://practicasdeverano.itam.mx/html/verificacion.html");
+      }
+
+      const btnFormulario = document.getElementById("btnFormulario");
+
+    //text
+    const txtAlumNom = document.getElementById("txtAlumNom");
+    const txtAlumCU = document.getElementById("txtAlumCU");
+    const txtAlumTel = document.getElementById("txtAlumTel");
+    const txtAlumCarrera = document.getElementById("txtAlumCarrera");
+    const txtAlumSem = document.getElementById("txtAlumSem");
+
+    const txtAlumInteres1 = document.getElementById("txtAlumInteres1");
+    const txtAlumInteres2 = document.getElementById("txtAlumInteres2");
+    const txtAlumInteres3 = document.getElementById("txtAlumInteres3");
+
+    btnFormulario.addEventListener("click",function(){
+      const nom = txtAlumNom.value;
+      const cU = txtAlumCU.value;
+      const tel = txtAlumTel.value;
+      const car = txtAlumCarrera.value;
+      const semestre = txtAlumSem.value;
+      const interes = txtAlumInteres1.value;
+      const interes2 = txtAlumInteres2.value;
+      const interes3 = txtAlumInteres3.value;
+
+      llenaEmpresa(nom,cU,tel,car,semestre,interes, interes2, interes3);
     }
-}
-    function displayMensaje() {
-      window.location.href = ("http://practicasdeverano.itam.mx/html/verificacion.html");
-    }
-
