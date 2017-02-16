746028473b578c4275761160968596fef009578d
uboat46 <labMovilesITAM@gmail.com>
2017-02-16 14:58:47 -0600

    version 1.2.2

--- pruebaFirebase/js/main.js
+++ pruebaFirebase/js/main.js
@@ -31,7 +31,36 @@ var config = {
     });
 
 
-    function displayIdioma() {
+    
+
+      const btnFormulario = document.getElementById("btnFormulario");
+
+    //text
+    const txtAlumNom = document.getElementById("txtAlumNom");
+    const txtAlumCU = document.getElementById("txtAlumCU");
+    const txtAlumTel = document.getElementById("txtAlumTel");
+    const txtAlumCarrera = document.getElementById("txtAlumCarrera");
+    const txtAlumSem = document.getElementById("txtAlumSem");
+
+    const txtAlumInteres1 = document.getElementById("txtAlumInteres1");
+    const txtAlumInteres2 = document.getElementById("txtAlumInteres2");
+    const txtAlumInteres3 = document.getElementById("txtAlumInteres3");
+
+    btnFormulario.addEventListener("click",function(){
+      const nom = txtAlumNom.value;
+      const cU = txtAlumCU.value;
+      const tel = txtAlumTel.value;
+      const car = txtAlumCarrera.value;
+      const semestre = txtAlumSem.value;
+      const interes = txtAlumInteres1.value;
+      const interes2 = txtAlumInteres2.value;
+      const interes3 = txtAlumInteres3.value;
+
+      llenaAlumno(nom,cU,tel,car,semestre,interes, interes2, interes3);
+    });
+});
+
+  function displayIdioma() {
       var strIdioma = $("#selectAlumIdiomas :selected").text();
       var btn = document.createTextNode(" de " + strIdioma);
       var btn2 ="";
@@ -78,33 +107,3 @@ var config = {
           document.getElementById("requiredNivel").required = true; 
         }
       }
-      function displayMensaje() {
-        window.location.href = ("http://practicasdeverano.itam.mx/html/verificacion.html");
-      }
-
-      const btnFormulario = document.getElementById("btnFormulario");
-
-    //text
-    const txtAlumNom = document.getElementById("txtAlumNom");
-    const txtAlumCU = document.getElementById("txtAlumCU");
-    const txtAlumTel = document.getElementById("txtAlumTel");
-    const txtAlumCarrera = document.getElementById("txtAlumCarrera");
-    const txtAlumSem = document.getElementById("txtAlumSem");
-
-    const txtAlumInteres1 = document.getElementById("txtAlumInteres1");
-    const txtAlumInteres2 = document.getElementById("txtAlumInteres2");
-    const txtAlumInteres3 = document.getElementById("txtAlumInteres3");
-
-    btnFormulario.addEventListener("click",function(){
-      const nom = txtAlumNom.value;
-      const cU = txtAlumCU.value;
-      const tel = txtAlumTel.value;
-      const car = txtAlumCarrera.value;
-      const semestre = txtAlumSem.value;
-      const interes = txtAlumInteres1.value;
-      const interes2 = txtAlumInteres2.value;
-      const interes3 = txtAlumInteres3.value;
-
-      llenaEmpresa(nom,cU,tel,car,semestre,interes, interes2, interes3);
-    }
-}
